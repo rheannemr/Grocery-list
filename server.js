64682aa3f46793e68b8943376b121241c83b0e5b
Tina Armstead <tina.armstead@gmail.com>
2021-02-23 23:35:18 -0500

    Added folder/file structure

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgers_controllers.js')
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function(){
+    console.log(`listening on port: ${PORT}`);
+});
+
