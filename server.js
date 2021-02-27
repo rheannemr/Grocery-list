f978def7472d3c16437710bb3e1c0e4b8194fdc4
Tina Armstead <tina.armstead@gmail.com>
2021-02-27 14:48:43 -0500

    Updtaed server.js file

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const routes = require('./routes/grocery_routes.js');
+
+// const htmlRouter = require('./routes/html-routes.js');
+// const authorRouter = require('./routes/author-api-routes.js');
+const apiRouter = require('./routes/post-api-routes.js');
+
+// Sets up the Express App
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080 ;
+// Requiring our models for syncing
+const db = require('./models');
 
-app.use(express.static(__dirname + '/public/'));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
 
+// Static directory
+app.use(express.static('public'));
 
-app.listen(PORT, function(){
-    console.log(`app is listening on port: ${PORT}`);
+// Invoke routes
+htmlRouter(app);
+authorRouter(app);
+apiRouter(app);
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 });
+
+
+
+
+
+// const express = require('express');
+// const exphbs = require('express-handlebars');
+// const routes = require('./routes/grocery_routes.js');
+// const app = express();
+
+// const PORT = process.env.PORT || 8080 ;
+
+// app.use(express.static(__dirname + '/public/'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true}));
+
+
+// app.listen(PORT, function(){
+//     console.log(`app is listening on port: ${PORT}`);
+// });
 
