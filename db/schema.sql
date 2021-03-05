00ee980128247b5ae69acb19ac79fc8ae98c6136
Rheanne Reyes <rheannemr@gmail.com>
2021-03-04 19:49:59 -0800

    Update sql DB schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,7 @@ USE grocerylist_db;
 
 CREATE TABLE groceries (
     id INT NOT NULL AUTO_INCREMENT,
-    food VARCHAR(50) NOT NULL,
-    quantity INT NOT NULL,
-    purchased BOOLEAN DEFAULT false,
+    name VARCHAR(50) NOT NULL,
+    inFridge BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );
